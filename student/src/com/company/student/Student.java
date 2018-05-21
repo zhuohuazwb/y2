5476ba8906aaae9908fbeaef5e26833d8da36d5a
wenbin <919323695@qq.com>
2018-05-21 15:46:35 +0800

    test

---
+++ student/src/com/company/student/Student.java
@@ -0,0 +1,10 @@
+package com.company.student;
+
+/**
+ * Created by Administrator on 2018/2/9/009.
+ */
+public class Student {
+   private int studentNo;
+   private String studentName;
+   private char sex;
+}
